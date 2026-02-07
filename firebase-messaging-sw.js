46e55802622918bb8dc8d3c43c13c0b15018ad5d
nchebani <143761934+nchebani@users.noreply.github.com>
2026-02-07 17:40:24 +0100

    Update Firebase messaging service worker script

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-sw.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-sw.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyAbcgDTWjOjog1pRDU9N5tnevD1MvfAG5Q",
+    authDomain: "live-hub-2026.firebaseapp.com",
     projectId: "live-hub-2026",
+    storageBucket: "live-hub-2026.firebasestorage.app",
     messagingSenderId: "56918867184",
     appId: "1:56918867184:web:7667d75288d14a565e9635"
 });
 
 const messaging = firebase.messaging();
 
+// Gère l'affichage quand l'app est en arrière-plan
 messaging.onBackgroundMessage((payload) => {
-  console.log('Push reçu en arrière-plan:', payload);
-  const notificationTitle = payload.notification.title || "Nouveau message !";
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/3119/3119338.png',
-    badge: 'https://cdn-icons-png.flaticon.com/512/3119/3119338.png'
-  };
+    console.log('Message reçu en arrière-plan:', payload);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icon.png' // Optionnel : ajoute un logo à la racine
+    };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
